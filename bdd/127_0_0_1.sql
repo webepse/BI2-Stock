6ca5dd1055cbfa09d1f3a71b1929927b31ce6549
Berti <berti@epse.be>
2022-11-18 09:49:39 +0100

    changement bdd

--- bdd/127_0_0_1.sql
+++ bdd/127_0_0_1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : jeu. 17 nov. 2022 à 11:40
+-- Généré le : ven. 18 nov. 2022 à 07:48
 -- Version du serveur : 5.7.36
 -- Version de PHP : 7.4.26
 
@@ -26,6 +26,28 @@ USE `stock`;
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `members`
+--
+
+DROP TABLE IF EXISTS `members`;
+CREATE TABLE IF NOT EXISTS `members` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `login` varchar(60) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `members`
+--
+
+INSERT INTO `members` (`id`, `login`, `password`, `email`) VALUES
+(1, 'admin', '$2y$10$.eoxfYKmAsO7x2MS9pXqouoPIVOL/td0DKG4ziZcPWhbp1nD0vlRq', 'admin@epse.be');
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `products`
 --
 
@@ -35,7 +57,7 @@ CREATE TABLE IF NOT EXISTS `products` (
   `title` varchar(100) NOT NULL,
   `description` text NOT NULL,
   `date` date NOT NULL,
-  `cover` varchar(255) NOT NULL,
+  `cover` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
